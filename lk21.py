25d9c8e7936baff36f893886d4c017c995452fcc
NOOBIE <44431105+KANG-NEWBIE@users.noreply.github.com>
2020-03-24 22:38:50 +0700

    Add files via upload

---
+++ lk21.py
@@ -0,0 +1,128 @@
+import requests,re,os,json,time
+from bs4 import BeautifulSoup as Bs
+from tqdm import tqdm
+
+ses=requests.Session()
+info={
+	'title':[],
+}
+
+def search(query):
+	c=1
+	req=ses.get('http://149.56.24.226/?s='+query)
+	bs=Bs(req.text, 'html.parser')
+	hsl=bs.find_all('div',{'class':'col-xs-9 col-sm-10 search-content'})
+	for i in hsl:
+		tit=i.find('a',{'rel':'bookmark'})
+		info['title'].append((tit.text, tit['href']))
+	if len(info['title']) == 0:
+		print("Tidak dapat menemukan judul film")
+		return True
+	
+	print("\n\t[ Result ]")
+	for x in info['title']:
+		print(f"{c}. {x[0]}")
+		c+=1
+
+	pil=int(input("_> pilih: "))
+	if pil <= 0:
+		print("index out of ranges")
+		return True
+
+	print(" *Bypassing, please wait...")
+	bypass(info['title'][pil-1][1], info['title'][pil-1][0])
+
+def bypass(url,judul):
+	cc=1
+	req=ses.get(url)
+	bs=Bs(req.text, 'html.parser')
+	link=bs.find('a',{'class':'btn btn-success'})['href']
+#	print(link)
+
+	req2=ses.get(link)
+	rg=re.findall(r'<frame src="(.*)">',req2.text)[0]
+#	print(rg)
+
+	blin=link.split('/')[2]
+	req3=ses.get(f"http://{blin}{rg}")
+	bs2=Bs(req3.text, 'html.parser')
+	link2=bs2.find('a',{'target':'_parent'})['href']
+#	print(link2)
+
+	req4=ses.get(link2)
+	rg2=re.findall(r'post\("(.*?)", {',req4.text)[0]
+#	print(rg2)
+
+	blin2=req4.url.split('/')[2]
+	head={
+	'Host':blin2,
+	'accept':'*/*',
+	'origin':f'http://{blin2}',
+	'x-requested-with':'XMLHttpRequest',
+	'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+	'content-type':'application/x-www-form-urlencoded;charser=UTF-8',
+	'referer':link2,
+	'accept-encoding':'gzip, deflate, br',
+	'accept-language':'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',
+	}
+	req5=ses.post(f"http://{blin2}{rg2}",headers=head)
+	rg3=re.findall(r'https://layarkacaxxi.org/f/(.*?)"',req5.text)[0]
+#	print(rg3)
+
+	req6=ses.get(f'https://layarkacaxxi.org/f/{rg3}')
+	try:
+		rg4=re.findall(r"post\('(.*?)', ",req6.text)[0]
+	except:
+		raise Exception("\nDCMATakedown: Video tidak tersedia")
+#	print(rg4)
+
+	req7=ses.post(f'https://layarkacaxxi.org{rg4}')
+	js=json.loads(req7.text)
+
+	print("\n\t[ Resulution ]")
+	for x in js['data']:
+		print(f"{cc}. {x['label']}")
+		cc+=1
+
+	lih=int(input("_> pilih: "))
+	if lih <= 0:
+		print("index out of ranges")
+		return True
+
+	downld2(js['data'][lih-1]['file'], judul)
+
+def downld2(url,judul):
+	r = ses.get(url, stream=True)
+	total_size = int(r.headers.get('content-length', 0))
+	print(f"\n# Downloading {judul}")
+	block_size = 1024
+	t=tqdm(total=total_size, unit='iB', unit_scale=True)
+	with open(f'result/{judul.replace("/",",")}.mp4','wb') as f:
+		for data in r.iter_content(block_size):
+			t.update(len(data))
+			f.write(data)
+	t.close()
+	if total_size != 0 and t.n != total_size:
+		print("\n[Warn] Download GAGAL")
+		tan=input("[?] anda ingin melanjutkannya ke website layarkaca21 (y/n) ")
+		if tan.lower() == 'y':
+			click.launch(info['title'][pil-1][1])
+		else:
+			sys.exit("okay bye bye:*")
+
+if __name__ == "__main__":
+	os.system('clear')
+	print("""
+	[ LayarKaca21 Downloader ]
+	      | by: Noobie |
+""")
+	try:
+		os.mkdir('result')
+	except: pass
+
+	try:
+		que=input("query search: ")
+		search(que)
+		print("\n[OK] file saved in result\n")
+	except Exception as Err:
+		print(Err)
